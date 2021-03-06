0bcbac826792d4fb8b81f3818e8c41ce8e7b855b
Melih FIRAT <melih.firat@ogr.sakarya.edu.tr>
2021-03-06 21:11:37 +0300

    Style, wellcome page and cursor blinker change.

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,20 +1,29 @@
 'use strict';
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
 //Typer Coded by github.com/firatmelih
 var [i, j, k] = [0, 0, 0];
-let txtArray = ['Hello World!', 'I am Melih', 'Wellcome to my page.', 'check my github ;)']
+let txtArray = ['a Developer', 'Programmer', 'Student', 'Melih']
 var txt = txtArray[k];
-var speed = 150;
+var speed = 200;
 var textToType = document.getElementById("typer");
 var cursor = document.getElementById("cursor");
 Typer();
 
-function Typer() {
+async function Typer() {
+    if (j == txt.length) {
+        await sleep(1000);
+    }
     if (i < txt.length) {
-        setTimeout(cursorBlink, speed * 2);
+        // setTimeout(cursorBlink, speed / 2);
         textToType.innerHTML += txt.charAt(i);
         i++;
-        setTimeout(Typer, speed);
+        setTimeout(Typer, speed, cursor.hidden = false);
         j = i;
     }
     else if (j > 0) {
@@ -34,6 +43,8 @@ function Typer() {
 }
 // to get a blinking cursor effect.
 function cursorBlink() {
-    if (cursor.hidden) cursor.hidden = false;
-    else cursor.hidden = true;
+    if (i < txt.length) {
+        if (cursor.hidden) cursor.hidden = false;
+        else cursor.hidden = true;
+    }
 }
